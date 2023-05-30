e6656f7044d5ab6eec3e88ad88deadf9e9f730a7
LiamHsu <s459958@gmail.com>
2023-05-30 16:39:42 +0800

    Increase end-to-end test case completion timeout sectond

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -47,7 +47,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 15.0)
         
         return receivedResult
     }
