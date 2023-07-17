cf38b142d9141d15a80098b62c13e3c754530cb9
LiamHsu <s459958@gmail.com>
2023-07-17 11:14:16 +0800

    Move main thread dispatch to the Composition layer with a Decorator.

--- EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -12,7 +12,7 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decorate: feedLoader))
         let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let feedController = FeedViewController(refreshController: refreshController)
         feedController.title = FeedPresenter.title
@@ -23,6 +23,26 @@ public final class FeedUIComposer {
         return feedController
     }
     
+}
+
+private final class MainQueueDispatchDecorator: FeedLoader {
+    private let decorate: FeedLoader
+    
+    init(decorate: FeedLoader) {
+        self.decorate = decorate
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decorate.load { result in
+            if Thread.isMainThread {
+                completion(result)
+            } else {
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            }
+        }
+    }
 }
 
 private final class WeakRefVirtualProxy<T: AnyObject> {
