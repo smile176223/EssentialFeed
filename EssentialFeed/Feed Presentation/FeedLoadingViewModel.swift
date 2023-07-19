97a7ebcfd80e9a90b2063178c63cf4e84e968131
LiamHsu <s459958@gmail.com>
2023-07-19 15:25:43 +0800

    Move `FeedViewModel` and `FeedLoadingViewModel` and `FeedErrorViewModel` to separate files

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Liam on 2023/7/19.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
