26ccf3d812733306569d3c7028f6f94542758a5e
LiamHsu <s459958@gmail.com>
2023-07-20 08:10:03 +0800

    Move `FeedImageViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeed
+//
+//  Created by Liam on 2023/7/20.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
