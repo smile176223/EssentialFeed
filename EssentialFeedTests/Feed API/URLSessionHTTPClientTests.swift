e8db724cb072a0dcddb9f40714834b3de13cc2fe
LiamHsu <s459958@gmail.com>
2023-07-25 10:38:09 +0800

    Extract duplicate `anyData` helper into a shared scope

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -92,10 +92,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         return sut
     }
     
-    private func anyData() -> Data {
-        return Data("any data".utf8)
-    }
-    
     private func nonHTTPURLResponse() -> URLResponse {
         return URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
     }
