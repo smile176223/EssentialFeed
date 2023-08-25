bd05677b39bb6a780ca01d802e4ec0f9044d2236
LiamHsu <s459958@gmail.com>
2023-08-25 15:52:52 +0800

    Rename and move ResourceErrorView and ViewModel to Shared module

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -17,7 +17,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     
     private let resourceView: View
     private let loadingView: ResourceLoadingView
-    private let errorView: FeedErrorView
+    private let errorView: ResourceErrorView
     private let mapper: Mapper
     
     public static var loadError: String {
@@ -28,7 +28,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
             comment: "Error message displayed when we can't load the image feed from the server")
     }
     
-    public init(resourceView: View, loadingView: ResourceLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper ) {
+    public init(resourceView: View, loadingView: ResourceLoadingView, errorView: ResourceErrorView, mapper: @escaping Mapper ) {
         self.resourceView = resourceView
         self.loadingView = loadingView
         self.errorView = errorView
@@ -46,7 +46,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     }
     
     public func didFinishLoading(with error: Error) {
-        errorView.display(FeedErrorViewModel(message: Self.loadError))
+        errorView.display(ResourceErrorViewModel(message: Self.loadError))
         loadingView.display(ResourceLoadingViewModel(isLoading: false))
     }
 }
---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by Liam on 2023/8/25.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
