fa87124f8b2efe192f9ac386722f314ec953fb0d
LiamHsu <s459958@gmail.com>
2023-08-28 13:42:04 +0800

    Rename FeedViewController to ListViewController since it can now render any collection or cell controllers

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewController.swift
+//  ListViewController.swift
 //  EssentialFeediOS
 //
 //  Created by Liam on 2023/7/6.
@@ -18,7 +18,7 @@ public protocol CellController {
     func cancelLoad()
 }
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
+public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
     @IBOutlet private(set) public var errorView: ErrorView?
     
     private var loadingControllers = [IndexPath: CellController]()
