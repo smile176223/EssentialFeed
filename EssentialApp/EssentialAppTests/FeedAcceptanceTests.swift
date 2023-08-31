7664dc6866df30d0f3cc9e1281d2ae8d6011e4c0
LiamHsu <s459958@gmail.com>
2023-08-31 11:12:58 +0800

    Fix Xcode 14.3.1 warning issue

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -132,7 +132,7 @@ class FeedAcceptanceTests: XCTestCase {
                 "author": [
                     "username": "a username"
                 ]
-            ],
+            ] as [String : Any]
         ]])
     }
     
